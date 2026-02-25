384a98ae351df4367512e8abd14e1219d628950f
Kim Aldwin Barona <48612594+Kimbarona@users.noreply.github.com>
2026-02-25 22:01:58 +0800

    Update VITE_API_URL

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://localhost:10000',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path
